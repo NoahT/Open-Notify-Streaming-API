c05ccf0b099f0a04d9ad03973c9f55fff63a0908
NoahT <noah.teshima@gmail.com>
2025-12-24 14:24:19 -0800

    Establish zookeeper ensemble based on FQDNs of pods in StatefulSet

--- src/ingestion/client/zookeeper/__init__.py
+++ src/ingestion/client/zookeeper/__init__.py
@@ -1,6 +1,6 @@
 from kazoo.client import KazooClient
 from .client import KazooZookeeperClient
 import logging
+import os
 
-
-client = KazooZookeeperClient(KazooClient(hosts='127.0.0.1:2181', logger=logging.getLogger(__name__)))
+client = KazooZookeeperClient(KazooClient(hosts=os.getenv('ZOO_SERVERS'), logger=logging.getLogger(__name__)))
