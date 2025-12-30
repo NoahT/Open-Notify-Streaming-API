bfd154a094e88d16987349e6bffce16e642b55a9
NoahT <noah.teshima@gmail.com>
2025-12-30 11:56:11 -0800

    Adjust main.py based on signal handling, delegate election loop to facade

--- src/ingestion/main.py
+++ src/ingestion/main.py
@@ -4,29 +4,72 @@ Main module for the ingestion service using ZooKeeper for leader election.
 
 import logging
 import os
+import threading
 
 from kazoo.client import KazooClient
 
-from .client.zookeeper.client import KazooZookeeperClient
-from .election.facade import SequentialEphemeralElectionFacade
+from .client.open_notify.client import OpenNotifyRequestsClient
+from .client.zookeeper.client import Client, KazooZookeeperClient
+from .election.facade import ElectionFacade, SequentialEphemeralElectionFacade
+from .util.signal_handler import SignalHandler, ZooKeeperSignalHandler
 
 
-def leadership_func():
-  logging.warning("Obtained leadership")
+def configure_loggging() -> None:
+  logging.basicConfig(level=logging.INFO,
+                      format='%(asctime)s %(levelname)s %(name)s: %(message)s')
 
 
-client = KazooZookeeperClient(kazoo_client=KazooClient(
-    hosts=os.getenv("ZOO_SERVERS"), logger=logging.getLogger(__name__)))
-client.start()
-election_facade = SequentialEphemeralElectionFacade(
-    zookeeper_client=client, logger=logging.getLogger(__name__))
-election_facade.connect()
+def get_open_notify_client() -> OpenNotifyRequestsClient:
+  open_notify_client = OpenNotifyRequestsClient()
+  return open_notify_client
 
-if __name__ == "__main__":
-  while True:
-    event = election_facade.check_leadership_status(leader_func=leadership_func)
 
-    if not event:
-      break
+def on_leadership_acquired(event: threading.Event) -> None:
+  iss_client = get_open_notify_client()
+  logging.warning('Obtained leadership')
+  while not event.is_set():
+    event.wait(5)
+    iss_location = iss_client.get_iss()
+    logging.warning('Short polling ISS location data [payload=%s]',
+                    iss_location)
 
-    event.wait()
+
+def get_zookeeper_client() -> KazooZookeeperClient:
+  kazoo_client = KazooClient(hosts=os.getenv('ZOO_SERVERS'),
+                             logger=logging.getLogger(__name__))
+  client = KazooZookeeperClient(kazoo_client=kazoo_client)
+
+  return client
+
+
+def get_facade(zookeeper_client: KazooZookeeperClient,
+               signal_handler: SignalHandler) -> ElectionFacade:
+  election_facade = SequentialEphemeralElectionFacade(
+      zookeeper_client=zookeeper_client,
+      signal_handler=signal_handler,
+      logger=logging.getLogger(__name__))
+  election_facade.connect()
+
+  return election_facade
+
+
+def get_signal_handler(zookeeper_client: Client) -> SignalHandler:
+  return ZooKeeperSignalHandler(zookeeper_client=zookeeper_client)
+
+
+def main() -> None:
+  configure_loggging()
+  zookeeper_client = get_zookeeper_client()
+  try:
+    zookeeper_client.start()
+    signal_handler = get_signal_handler(zookeeper_client=zookeeper_client)
+    election_facade = get_facade(zookeeper_client=zookeeper_client,
+                                 signal_handler=signal_handler)
+    election_facade.run_leader_election_loop(
+        on_leadership_acquired=on_leadership_acquired)
+  finally:
+    zookeeper_client.stop()
+
+
+if __name__ == '__main__':
+  main()
