cab92e8159f7f41f8bce58445a05ad0b0d46b345
NoahT <noah.teshima@gmail.com>
2026-01-03 11:42:34 -0800

    Add remaining unit tests for Config abstract methods, including tests for null values and config overrides

--- test/unit_tests/ingestion/config/test_config_facade.py
+++ test/unit_tests/ingestion/config/test_config_facade.py
@@ -27,3 +27,23 @@ class ConfigFacadeTestSuite(unittest.TestCase):
   def test_should_return_list_in_config(self) -> None:
     value = self._config_facade.read_list('SOME_KEY_FOR_LIST')
     self.assertEqual([1, 2, 3], value)
+
+  def test_should_return_dict_in_config(self) -> None:
+    value = self._config_facade.read_dict('SOME_KEY_FOR_DICT')
+    self.assertEqual({"SOME_KEY_FOR_STR": "some value"}, value)
+
+  def test_should_return_int_in_config(self) -> None:
+    value = self._config_facade.read_int('SOME_KEY_FOR_INT')
+    self.assertEqual(12345, value)
+
+  def test_should_return_float_in_config(self) -> None:
+    value = self._config_facade.read_float('SOME_KEY_FOR_FLOAT')
+    self.assertEqual(1.2345, value)
+
+  def test_should_return_overridden_value(self) -> None:
+    value = self._config_facade.read_str('SOME_KEY_FOR_OVERRIDE')
+    self.assertEqual('This value should be set', value)
+
+  def test_should_return_default_for_null_value(self) -> None:
+    value = self._config_facade.read_str('SOME_KEY_FOR_NONE', 'my_default')
+    self.assertEqual('my_default', value)
