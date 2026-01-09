6d55a3c610edbc4430afa3efbb9d12afa558d5ac
NoahT <noah.teshima@gmail.com>
2026-01-08 20:59:52 -0800

    Added integration test suite for Open Notify client

---
+++ test/integration_tests/ingestion/client/open_notify/test_client.py
@@ -0,0 +1,22 @@
+import unittest
+
+from src.ingestion.client.open_notify.client import OpenNotifyRequestsClient
+
+
+class OpenNotifyRequestsClientTestSuite(unittest.TestCase):
+  '''
+  Test suite for OpenNotifyRequestsClient.
+  '''
+
+  def setUp(self) -> None:
+    self._client = OpenNotifyRequestsClient()
+
+  def test_should_return_iss_data_on_successful_request(self) -> None:
+    response = self._client.get_iss()
+
+    self.assertIsNotNone(response)
+    self.assertIsNotNone(response['timestamp'])
+    self.assertEqual('success', response['message'])
+    self.assertIsNotNone(response['iss_position'])
+    self.assertIsNotNone(response['iss_position']['latitude'])
+    self.assertIsNotNone(response['iss_position']['longitude'])
