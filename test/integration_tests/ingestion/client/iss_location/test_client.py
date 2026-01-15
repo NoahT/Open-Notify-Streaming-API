ad642b2754cc3a6114b1b60962414fa7f613a030
NoahT <noah.teshima@gmail.com>
2026-01-14 21:42:21 -0800

    Containerize integration tests and Firestore emulator

--- test/integration_tests/ingestion/client/iss_location/test_client.py
+++ test/integration_tests/ingestion/client/iss_location/test_client.py
@@ -13,14 +13,19 @@ class ISSLocationFirestoreClientTestSuite(unittest.TestCase):
 
   @classmethod
   def setUpClass(self) -> None:
-    # Same database; I really just didn't want to move out of free tier. In the future we
-    # might consider installing the Firestore emulator and ITs with Docker compose so
-    # we can run the integration tests in a more isolated manner.
+    # Same database; I really just didn't want to move out of free tier.
+    # In the future wemight consider installing the Firestore emulator and
+    # ITs with Docker compose so we can run the integration tests in a more
+    # isolated manner.
     self._config = ConfigFacade(
         'test/integration_tests/ingestion/client/iss_location', 'test',
         'default')
     self._client = ISSLocationFirestoreClient(
         config=self._config, collection_name='iss_locations_it')
+
+  @classmethod
+  def tearDownClass(cls):
+    return super().tearDownClass()
 
   def test_should_add_iss_location_documents_correctly(self) -> None:
     iss_location_obj = ISSLocation(ts=int(time.time()),
@@ -43,8 +48,8 @@ class ISSLocationFirestoreClientTestSuite(unittest.TestCase):
 
     ts_to = int(time.time())
     ts_from = ts_to - 10
-    # Rough window. Could fail due to concurrent runs. Again, can be fixed in future if
-    # emulator inside container is used for integration tests.
+    # Rough window. Could fail due to concurrent runs. Again, can be fixed
+    # in future if emulator inside container is used for integration tests.
     iss_location_documents = self._client.get_iss_locations(ts_from=ts_from,
                                                             ts_to=ts_to)
 
