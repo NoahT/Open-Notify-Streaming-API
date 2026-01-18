bacae18cdef8310bb33245ad5cf123e5d8c5e346
NoahT <noah.teshima@gmail.com>
2026-01-18 13:59:46 -0800

    Added redis client for publishing ISS location updates

---
+++ src/ingestion/client/publisher/client.py
@@ -0,0 +1,78 @@
+''' Client for publishing ISS location updates. '''
+import json
+import logging
+from abc import ABC, abstractmethod
+
+import redis
+
+from ...config.config_facade import Config
+from ..iss_location.iss_location import ISSLocation
+
+
+class PublisherClient(ABC):
+  '''
+  Abstract base class for publishing ISS location updates to a
+  messaging broker.
+  '''
+
+  @abstractmethod
+  def publish_iss_location(self, iss_location: ISSLocation) -> bool:
+    '''
+    Publish an ISS location update.
+    
+    :param self: Current PublisherClient instance.
+    :param iss_location: The ISS location update to publish.
+    :type iss_location: ISSLocation
+    :return: Whether the message successfully published.
+    :rtype: bool
+    '''
+    pass
+
+
+class RedisPublisherClient(PublisherClient):
+  '''
+  Redis based publisher client for ISS location updates.
+  '''
+
+  def __init__(self,
+               config: Config,
+               logger: logging.Logger = logging.getLogger(__name__)):
+    self._client = None
+    self._config = config.read_dict('REDIS_CLIENT')
+    self._logger = logger
+
+  def publish_iss_location(self, iss_location: ISSLocation) -> bool:
+    is_published = False
+    channel = self._config['CHANNEL']
+    message = json.dumps(iss_location.iss_dict)
+    try:
+      subscribers = self.client.publish(channel=channel, message=message)
+
+      self._logger.info(
+          ('Published ISS location update ', f'[channel={channel}, ',
+           f'message={message}, ', f'subscribers={subscribers}]'))
+      is_published = True
+    #pylint: disable=broad-exception-caught
+    except Exception as exception:
+      self._logger.error(
+          ('Failed publishing ISS location update ', f'[channel={channel}, ',
+           f'message={message}, ', f'error={exception}]'))
+
+    return is_published
+
+  @property
+  def client(self) -> redis.Redis:
+    if not self._client:
+      host = self._config['HOST']
+      port = self._config['PORT']
+      connect_timeout = self._config['CONNECT']['TIMEOUT']
+      publish_timeout = self._config['PUBLISH']['TIMEOUT']
+
+      self._logger.info(f'Initialized Redis client [host={host}, port={port}]')
+
+      self._client = redis.Redis(host=host,
+                                 port=port,
+                                 socket_connect_timeout=connect_timeout,
+                                 socket_timeout=publish_timeout)
+
+    return self._client
---
+++ test/unit_tests/ingestion/client/publisher/test_client.py
@@ -0,0 +1,65 @@
+''' Unit test module for PublisherClient implementations.'''
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+import redis
+
+from src.ingestion.client.iss_location.iss_location import ISSLocation
+from src.ingestion.client.publisher.client import RedisPublisherClient
+from src.ingestion.config.config_facade import Config
+
+
+class RedisPublisherClientTestSuite(TestCase):
+  '''
+  Unit test suite for RedisPublisherClient.
+  '''
+
+  def setUp(self):
+    self._config = MagicMock(spec=Config)
+    self._config.read_dict.return_value = {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'CONNECT': {
+            'TIMEOUT': 5.000
+        },
+        'PUBLISH': {
+            'TIMEOUT': 5.000
+        },
+        'CHANNEL': 'iss_location',
+        'DECODE_RESPONSES': True
+    }
+    self._client = RedisPublisherClient(config=self._config)
+
+  @patch(target='redis.Redis')
+  def test_should_return_true_when_iss_location_data_published_successfully(
+      self, redis_patch: redis.Redis) -> None:
+    redis_mock = MagicMock()
+    redis_mock.publish.return_value = 1
+    redis_patch.return_value = redis_mock
+
+    iss_location = ISSLocation.from_dict({
+        'ts': 123,
+        'pos_la': 4.56,
+        'pos_lo': 7.89
+    })
+
+    is_published = self._client.publish_iss_location(iss_location=iss_location)
+
+    self.assertTrue(is_published)
+
+  @patch(target='redis.Redis')
+  def test_should_return_false_when_iss_location_data_published_unsuccessfully(
+      self, redis_patch: redis.Redis) -> None:
+    redis_mock = MagicMock()
+    redis_mock.publish.side_effect = RuntimeError('Redis publisher error')
+    redis_patch.return_value = redis_mock
+
+    iss_location = ISSLocation.from_dict({
+        'ts': 123,
+        'pos_la': 4.56,
+        'pos_lo': 7.89
+    })
+
+    is_published = self._client.publish_iss_location(iss_location=iss_location)
+
+    self.assertFalse(is_published)
