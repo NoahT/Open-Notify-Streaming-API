164eef05feb397d470b0b70958aae97416627ce1
Noah Teshima <noah.teshima@gmail.com>
2026-01-21 23:14:47 -0800

    Added redis subscriber client for ISS location updates (#37)
    * Added redis subscriber client for ISS location updates
    
    * Resolve pylint failures
    
    * Re-introduce required positional argument in ITs
    
    * Make threading process finite to avoid hanging CI/CD
    
    * Remove threading to avoid CI/CD hanging
    
    * Add redis to dependent services
    
    * Added method to close the pub/sub connection

---
+++ src/streaming/client/subscriber/client.py
@@ -0,0 +1,68 @@
+''' Module for ISS location subscriber implementations. '''
+import logging
+from abc import ABC, abstractmethod
+
+import redis
+
+from ...config.config_facade import Config
+
+
+class SubscriberClient(ABC):
+  '''
+  Abstract base class for subscribing to ISS location updates.
+  '''
+
+  @abstractmethod
+  def subscribe_iss_location(self, handler) -> None:
+    '''
+    Abstract method used to add an async handler for ISS location 
+    updates.
+    
+    :param self: Current SubscriberClient instance.
+    :param handler: The async handler to use for ISS location updates.
+    :type handler: function
+    '''
+    pass
+
+
+class RedisSubscriberClient(SubscriberClient):
+  '''
+  SusbscriberClient implementation which uses Redis channels to subscribe to 
+  ISS location updates.
+  '''
+
+  def __init__(
+      self,
+      config: Config,
+      logger: logging.Logger = logging.getLogger(__name__)) -> None:
+    self._config = config.read_dict('REDIS_CLIENT')
+    self._logger = logger
+    self._client = None
+    self._subscriber_thread = None
+
+  def subscribe_iss_location(self, handler) -> None:
+    pubsub = self.client.pubsub()
+    channel = self._config['CHANNEL']
+    sleep_time = self._config['SUBSCRIBE']['SLEEP_TIME']
+    pubsub.subscribe(**{channel: handler})
+    self._subscriber_thread = pubsub.run_in_thread(sleep_time=sleep_time)
+
+    self._logger.info(
+        f'Subscribed to channel [channel={channel}, sleep_time={sleep_time}]')
+
+  def close_channel(self) -> None:
+    self._subscriber_thread.stop()
+
+  @property
+  def client(self) -> redis.Redis:
+    if not self._client:
+      host = self._config['HOST']
+      socket_connect_timeout = self._config['CONNECT']['TIMEOUT']
+      decode_responses = self._config['DECODE_RESPONSES']
+      self._client = redis.Redis(host=host,
+                                 socket_connect_timeout=socket_connect_timeout,
+                                 decode_responses=decode_responses)
+
+      self._logger.info(f'Initialized redis subscriber [host={host}]')
+
+    return self._client
---
+++ test/integration_tests/streaming/client/subscriber/test_client.py
@@ -0,0 +1,46 @@
+''' Module for integration testing SubscriberClient implementations. '''
+import logging
+import time
+from unittest import TestCase
+
+from src.ingestion.client.publisher.client import RedisPublisherClient
+from src.streaming.client.subscriber.client import RedisSubscriberClient
+from src.streaming.client.subscriber.iss_location import ISSLocation
+from src.streaming.config.config_facade import ConfigFacade
+
+
+class RedisSubscriberClientTestSuite(TestCase):
+  '''
+  Integration test suite for RedisSubscriberClient.
+  '''
+
+  def setUp(self):
+    self._config = ConfigFacade(
+        config_path='./test/integration_tests/streaming/client/subscriber',
+        config_env='test',
+        config_env_default='default')
+    self._client_subscriber = RedisSubscriberClient(config=self._config)
+    self._client_publisher = RedisPublisherClient(config=self._config)
+
+  def test_should_correctly_consume_messages_from_iss_channel(self) -> None:
+    self._is_consumed = False
+
+    def handler(message):
+      logging.warning('Message consumed: %s', message['data'])
+      self._is_consumed = True
+
+    self._client_subscriber.subscribe_iss_location(handler=handler)
+
+    iss_location = ISSLocation.from_dict({
+        'ts': int(time.time()),
+        'pos_la': 1.2345,
+        'pos_lo': 6.7890
+    })
+    self._client_publisher.publish_iss_location(iss_location=iss_location)
+
+    time.sleep(5)
+
+    self.assertTrue(self._is_consumed)
+
+  def tearDown(self):
+    self._client_subscriber.close_channel()
