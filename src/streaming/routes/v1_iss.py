7477de82d920fcf69dd87eb5a4f0ff0447c7d2a8
NoahT <noah.teshima@gmail.com>
2026-02-03 22:31:26 -0800

    Added integration test suite for v1/iss/stream and v1/iss/location

--- src/streaming/routes/v1_iss.py
+++ src/streaming/routes/v1_iss.py
@@ -1,8 +1,7 @@
 ''' Module handling v1 based routing '''
-import json
-
-from flask import Blueprint, Response, request
+from flask import Blueprint, request
 from flask_sse import ServerSentEventsBlueprint
+from werkzeug.exceptions import HTTPException
 
 from ..middleware.iss_controller import V1ISSController
 
@@ -14,13 +13,18 @@ def create_v1_blueprint(controller: V1ISSController) -> Blueprint:
 
   @v1_blueprint.route('/iss/location', methods=['GET'])
   def get_events():
-    window = request.args.get('window')
-    events = controller.v1_iss_events(window=window)
-    response_json = json.dumps(events)
-    status = 200
-    response = Response(response=response_json, status=status)
-
+    window = request.args.get('window', default=30, type=int)
+    response = controller.v1_iss_events(window=window)
     return response
+
+  @v1_blueprint.errorhandler(HTTPException)
+  def error_handler(exception: HTTPException):
+    response_json = {
+        'description': exception.description,
+        'message': exception.response
+    }
+
+    return (response_json, exception.code)
 
   v1_blueprint.add_url_rule(rule='/iss/stream',
                             endpoint='_iss_stream',
--- test/integration_tests/streaming/__init__.py
+++ test/integration_tests/streaming/__init__.py
@@ -1,1 +1,27 @@
+''' Init module for common dependencies in integration tests. '''
 
+import time
+from test.integration_tests.streaming.config import config
+
+from iss_location_client.client import ISSLocationFirestoreClient
+from iss_location_client.iss_location import ISSLocation
+
+from src.ingestion.client.publisher.client import RedisPublisherClient
+
+iss_client = ISSLocationFirestoreClient(config=config)
+
+iss_location1 = ISSLocation.from_dict({
+    'ts': int(time.time() - 5),
+    'pos_la': 1.23,
+    'pos_lo': 4.56
+})
+iss_client.add_iss_location(iss_location=iss_location1)
+
+iss_location2 = ISSLocation.from_dict({
+    'ts': (int(time.time()) - 10),
+    'pos_la': 7.89,
+    'pos_lo': 0.12
+})
+iss_client.add_iss_location(iss_location2)
+
+publisher_client = RedisPublisherClient(config=config)
