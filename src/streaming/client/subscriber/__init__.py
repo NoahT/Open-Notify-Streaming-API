bd4298d24f3033a5acd533901d44c4f8e1da7634
NoahT <noah.teshima@gmail.com>
2026-02-07 23:22:34 -0800

    Remove excesss subscriber code; propagate sse events from ingestion

--- src/streaming/client/subscriber/__init__.py
+++ src/streaming/client/subscriber/__init__.py
@@ -5,8 +5,7 @@ from cfg_environ.config import Config
 from .client import RedisSubscriberClient, SubscriberClient
 
 
-def get_subscriber(handler, config: Config) -> SubscriberClient:
+def get_subscriber(config: Config) -> SubscriberClient:
   redis_subscriber = RedisSubscriberClient(config=config)
-  redis_subscriber.subscribe_iss_location(handler=handler)
 
   return redis_subscriber
--- src/streaming/main.py
+++ src/streaming/main.py
@@ -3,21 +3,12 @@
   Module using Flask SSE for streaming events to connected clients.
 '''
 from .client import get_iss_client
-from .client.subscriber import get_subscriber
 from .config import get_config
 from .middleware import get_iss_repository, get_v1_controller
-from .routes import get_flask_app, on_iss_location_update
-
-
-def iss_location_update_handler(message: dict):
-  ''' Wrap the on_iss_location_update event handler in a closure. '''
-  with app.app_context():
-    on_iss_location_update(message=message)
-
+from .routes import get_flask_app
 
 config = get_config()
 client = get_iss_client(config=config)
 iss_repository = get_iss_repository(client=client)
 v1_controller = get_v1_controller(repository=iss_repository, config=config)
-subscriber = get_subscriber(handler=iss_location_update_handler, config=config)
 app = get_flask_app(controller=v1_controller, config=config)
