f9291ba470cebdfa63c5a9254c05bf32b5aab6f6
NoahT <noah.teshima@gmail.com>
2026-03-28 22:12:27 -0700

    Update cfg_environ usage in init module

--- src/ingestion/config/__init__.py
+++ src/ingestion/config/__init__.py
@@ -1,9 +1,18 @@
 ''' Init module for configuration. '''
 
+import os
+
 from cfg_environ.config import Config, ConfigFacade
+
+CONFIG_PATH = 'ingestion/config'
+CONFIG_ENV = 'development'
+CONFIG_ENV_DEFAULT = 'default'
 
 
 def get_config() -> Config:
-  config = ConfigFacade('ingestion/config', 'development', 'default')
+  config_env = os.getenv('ENVIRONMENT') or CONFIG_ENV
+  config = ConfigFacade(config_path=CONFIG_PATH,
+                        config_env=config_env,
+                        config_env_default=CONFIG_ENV_DEFAULT)
 
   return config
