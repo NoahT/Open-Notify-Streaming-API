fd9bd932e358b29a3b37b1e7fabb581c0f0eec65
NoahT <noah.teshima@gmail.com>
2026-03-28 22:47:31 -0700

    Add datastore.user role to service account in GKE

--- infra/terraform/service_account.tf
+++ infra/terraform/service_account.tf
@@ -3,9 +3,15 @@ resource "google_service_account" "default" {
   display_name = "Service Account"
 }
 
-resource "google_project_iam_member" "default_service_account_role_grant" {
+resource "google_project_iam_member" "default_service_account_artifact_registry_grant" {
   project = var.project
   role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+resource "google_project_iam_member" "default_service_account_firestore_grant" {
+  project = var.project
+  role    = "roles/datastore.user"
   member  = "serviceAccount:${google_service_account.default.email}"
 }
 
